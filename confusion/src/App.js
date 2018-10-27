c94ae66cf4d01f22ab108b26e8fe98dc4cc95bde
Supriya Verma <supriya.km@mindfiresolutions.com>
2018-10-27 23:35:57 +0530

    Create shared folder and move menu component into it
    app.js - create constructor and add props

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -3,8 +3,18 @@ import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+
+
+constructor(props){
+  super(props);
+
+  this.state={
+  dishes:DISHES
+}}
+
   render() {
     return (
       <div>
@@ -16,7 +26,7 @@ class App extends Component {
         </div>
         </Navbar>
 
-        <Menu/>
+        <Menu DISHES={this.state.dishes}/>
       </div>
     );
   }
