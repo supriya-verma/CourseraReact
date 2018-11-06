2fcb4821d25b8249e09ff91670818652fd74afb8
Supriya Verma <supriya.km@mindfiresolutions.com>
2018-11-06 20:51:04 +0530

    dispatch action via mapDispatchToProps

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
-
+import { addComment } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
  return {
@@ -18,6 +18,11 @@ const mapStateToProps = state => {
    leaders: state.leaders
  };
 }
+
+const mapDispatchToProps = (dispatch) => ({  
+  addComment: (dishId, rating, author, comment) => 
+  dispatch(addComment(dishId, rating, author, comment))
+});
 
 
 class Main extends Component {
@@ -62,4 +67,4 @@ constructor(props){
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
